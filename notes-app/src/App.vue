0ec0520233f7a490421d5e9358b551779eefebd8
Wang55566 <wang6@hawaii.edu>
2024-04-04 23:32:15 -0700

    css&html

--- notes-app/src/App.vue
+++ notes-app/src/App.vue
@@ -8,7 +8,10 @@
         </header>
         <div class="card-container">
           <div class="card">
-            <p class="main-text"></p>
+            <p class="main-text">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Voluptates illum excepturi, earum magnam iusto culpa!
+            </p>
             <p class="date"></p>
           </div>
         </div>
@@ -17,10 +20,53 @@
   </div>
 </template>
 
-<script setup>
+<script setup></script>
 
-</script>
+<style scoped>
+main {
+  height: 100vh;
+  width: 100vw;
+}
 
-<style lang="scss" scoped>
+.container {
+  max-width: 1000px;
+  padding: 10px;
+  margin: 0 auto;
+}
 
+header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+
+h1 {
+  font-weight: bold;
+  margin-bottom: 25px;
+  font-size: 75px;
+}
+
+button {
+  border: none;
+  padding: 10px;
+  width: 50px;
+  height: 50px;
+  cursor: pointer;
+  background-color: rgb(21, 20, 20);
+  color: white;
+  font-size: 20px;
+}
+
+.card {
+  width: 225px;
+  height: 225px;
+  background-color: rbg(237, 182, 44);
+  padding: 10px;
+  border-radius: 15px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  margin-right: 20px;
+  margin-bottom: 20px;
+}
 </style>
