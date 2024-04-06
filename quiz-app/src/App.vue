f40a54b9ce4b2f30d8929593f099c86811f3e4bd
Wang55566 <wang6@hawaii.edu>
2024-04-05 20:28:52 -0700

    commit

---
+++ quiz-app/src/App.vue
@@ -0,0 +1,80 @@
+<template>
+  <div class="container">
+    <header>
+      <h1>Quizes</h1>
+      <input type="text" placeholder="Search..." />
+    </header>
+    <div class="optionscontainer">
+      <div class="card">
+        <img
+          src="https://s.yimg.com/ny/api/res/1.2/MbmDmZ9Fv5hWlKH0rbkscQ--/YXBwaWQ9aGlnaGxhbmRlcjt3PTk2MDtoPTEyMTI7Y2Y9d2VicA--/https://media.zenfs.com/zh-tw/setn.com.tw/fec882a9c65f83ecdc5670e6a6513462"
+          alt=""
+        />
+        <div class="card-text">
+          <h2>Math</h2>
+          <p>15 questions</p>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup></script>
+
+<style scoped>
+.container {
+  max-width: 1000px;
+  margin: 0 auto;
+}
+
+header {
+  margin-bottom: 10px;
+  margin-top: 30px;
+  display: flex;
+  align-items: center;
+}
+
+header h1 {
+  font-weight: bold;
+  margin-right: 30px;
+}
+
+header input {
+  border: none;
+  background-color: rgba(128, 128, 128, 0.1);
+  padding: 10px;
+  border-radius: 5px;
+}
+
+.options-container {
+  display: flex;
+  flex-wrap: wrap;
+  margin-top: 40px;
+}
+
+/* CARD */
+
+.card {
+  width: 310px;
+  overflow: hidden;
+  border-radius: 2%;
+  box-shadow: 1px 1px 10px rgba(0, 0, 0, 0.1);
+  margin-bottom: 35px;
+  margin-right: 20px;
+  cursor: pointer;
+}
+
+.card img {
+  width:100%;
+  height: 190px;
+  margin: 0;
+}
+
+.card .card-text {
+  padding: 0 5px;
+}
+
+.card .card-text h2 {
+  font-weight: bold;
+}
+</style>
